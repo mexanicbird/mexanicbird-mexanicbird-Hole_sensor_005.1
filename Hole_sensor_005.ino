07a892bce4089c6fd89954739143d2755e94719c
Mexanicbird <mexanicbird@gmail.com>
2020-12-06 14:58:37 +0300

    commit_06.12.2020_008

--- Hole_sensor_005.ino
+++ Hole_sensor_005.ino
@@ -3,7 +3,9 @@ unsigned long lastflash;
 int RPM;
 int RPM_1;
 int RPM_2;
-GMedian<10, int> testFilter; 
+GMedian<20, int> testFilter; 
+GFilterRA average(0.5, 80);
+
 void setup() {
     Serial.begin(115200);  //открыть порт
     attachInterrupt(0,sens,RISING); //подключить прерывание на 2 пин при повышении сигнала
@@ -18,6 +20,7 @@ void sens() {
     if (RPM < RPM_1 + 2000 && RPM > RPM_1 - 2000){
     RPM_1 = RPM;
     RPM_2 = testFilter.filtered(RPM_1);
+    RPM_2 = average.filtered(RPM_2);
     }
     }
 }
@@ -25,8 +28,6 @@ void loop() {
   if ((micros()-lastflash)>500000){ //если сигнала нет больше секунды
     RPM_1=0;  //считаем что RPM 0
   }
-  
-  //Serial.println(RPM_1);   //вывод в порт
   Serial.println(RPM_2);   //вывод в порт
   delay(100);  //задержка для стабильности
 }
